1c4aea31ceac1afdfd9c2559dd25970ab4a03fc1
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-09-20 15:13:41 -0300

    Update cadastro.php

--- src/cadastro.php
+++ src/cadastro.php
@@ -1,4 +1,6 @@
 <?php
+$mensagemErro = ""; // Inicializa a mensagem de erro como uma string vazia
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $email = $_POST["email"];
@@ -10,31 +12,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Validação do nome
     if (empty($nome) || strpos($nome, " ") === false) {
-        echo "Preencha seu nome completo com sobrenome.<br>";
+        $mensagemErro .= "Preencha seu nome completo com sobrenome.<br>";
         $erro = true;
     }
 
     // Validação do email
     if (strlen($email) < 8 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Preencha um email válido com pelo menos 8 caracteres.<br>";
+        $mensagemErro .= "Preencha um email válido com pelo menos 8 caracteres.<br>";
         $erro = true;
     }
 
     // Validação da senha
     if (strlen($senha) < 8) {
-        echo "A senha deve ter pelo menos 8 caracteres.<br>";
+        $mensagemErro .= "A senha deve ter pelo menos 8 caracteres.<br>";
         $erro = true;
     }
 
     // Validação do CPF (pode ser mais rigorosa)
     if (empty($cpf) || !is_numeric($cpf) || strlen($cpf) != 11) {
-        echo "Preencha um CPF válido com 11 dígitos numéricos.<br>";
+        $mensagemErro .= "Preencha um CPF válido com 11 dígitos numéricos.<br>";
         $erro = true;
     }
 
     // Validação da data de nascimento (pode ser mais rigorosa)
     if (empty($data_nascimento) || !strtotime($data_nascimento)) {
-        echo "Preencha uma data de nascimento válida no formato YYYY-MM-DD.<br>";
+        $mensagemErro .= "Preencha uma data de nascimento válida no formato YYYY-MM-DD.<br>";
         $erro = true;
     }
 
@@ -50,7 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        echo "Este endereço de email já está cadastrado.<br>";
+        $mensagemErro .= "Este endereço de email já está cadastrado.<br>";
         $erro = true;
     }
 
@@ -65,11 +67,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->bind_param("sssss", $nome, $email, $senhaHash, $cpf, $data_nascimento);
 
         if ($stmt->execute()) {
-            echo "O usuário foi cadastrado com sucesso. Redirecionando para a página inicial...";
-            header("Refresh: 1; URL=index.html"); // Redireciona após 1 segundo
-
+            header("Location: index.html"); // Redireciona para a página inicial após o cadastro bem-sucedido
+            exit;
         } else {
-            echo "Erro ao inserir dados: " . $stmt->error;
+            $mensagemErro .= "Erro ao inserir dados: " . $stmt->error;
         }
 
         $stmt->close();
@@ -78,3 +79,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mysqli->close();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Página de Cadastro</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Cadastro</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" required placeholder="Digite seu nome">
+            
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" required placeholder="Digite seu e-mail">
+            
+            <label for="senha">Senha:</label>
+            <input type="password" id="senha" name="senha" required placeholder="Digite sua senha">
+            
+            <label for="cpf">CPF:</label>
+            <input type="text" id="cpf" name="cpf" required placeholder="Digite seu CPF">
+
+            <label for="data_nascimento">Data de Nascimento:</label>
+            <input type="date" id="data_nascimento" name="data_nascimento" required>
+            
+            <button type="submit">Cadastrar</button>
+        </form>
+
+        <p>É um profissional? <a href="cadastro_func.html">Cadastrar-se</a>.</p>
+        <p>Já tem uma conta? <a href="login.html">Fazer login</a>.</p>
+        <div id="mensagemErro"><?php echo $mensagemErro; ?></div>
+    </div>
+</body>
+</html>
