0c69eb6236e4c749a4a2454ac1d98ed9fb47afd6
PedroHenriqueNM <136659414+PedroHenriqueNM@users.noreply.github.com>
2023-09-22 11:49:32 -0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -9,32 +9,36 @@ DROP TABLE IF EXISTS Cliente;
 
 CREATE TABLE Cliente (
     idCliente INT AUTO_INCREMENT,
-    nome VARCHAR(200) NOT NULL,
+    nome_cliente VARCHAR(200) NOT NULL,
     email VARCHAR(200) NOT NULL,
     senha VARCHAR(200) NOT NULL,
-    data_nascimento DATE,
-    cpf VARCHAR(14)
-
+    data_nascimento DATE NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    Genero VARCHAR(50) NOT NULL,
+    
+    
     primary key(idCliente)
 );
 
 
 CREATE TABLE Funcionario (
     idFuncionario int NOT NULL AUTO_INCREMENT,
-    nome varchar(200) NOT NULL,
+    nome_funcionario varchar(200) NOT NULL,
     email varchar(200) NOT NULL,
     senha varchar(200) NOT NULL,
     data_nascimento DATE NOT NULL,
     cpf varchar(11) NOT NULL,
     cargo varchar(50) NOT NULL,
+    Genero VARCHAR(50) NOT NULL,
 
     primary key(idFuncionario)
 );
 
 CREATE TABLE Agendamento (
     idAgendamento int NOT NULL AUTO_INCREMENT,
-    cliente_nome varchar(200) NOT NULL,
-    funcionario_nome varchar(200) NOT NULL,
+    nome_servico varchar(200) NOT NULL FOREIGN KEY REFERENCES Servico(nome_servico),
+    nome_funcionario varchar(200) NOT NULL FOREIGN KEY REFERENCES Funcionario(nome_funcionario),
+    nome_cliente varchar(200) NOT NULL FOREIGN KEY REFERENCES Cliente(nome_cliente),
     data_consulta DATE NOT NULL,
     horario_consulta TIME NOT NULL,
 
@@ -43,7 +47,7 @@ CREATE TABLE Agendamento (
 
 CREATE TABLE Servico (
     idServico int NOT NULL AUTO INCREMENT,
-    nome varchar(200) NOT NULL,
+    nome_servico varchar(200) NOT NULL,
     descricao varchar(200) NOT NULL,
 
     primary key(idServico)
