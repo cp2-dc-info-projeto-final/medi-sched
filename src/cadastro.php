0d121efd7997d0c5078b846518f5af67012b205e
faelandrd14 <142700631+faelandrd14@users.noreply.github.com>
2023-09-25 13:57:30 -0300

    Update cadastro.php

--- src/cadastro.php
+++ src/cadastro.php
@@ -7,6 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $senha = $_POST["senha"];
     $cpf = $_POST["cpf"];
     $data_nascimento = $_POST["data_nascimento"];
+    $genero = $_POST["genero"]; // Adiciona o campo de gênero
     
     $erro = false; // Inicializar a variável $erro
 
@@ -30,13 +31,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Validação do CPF
     if (empty($cpf) || !is_numeric($cpf) || strlen($cpf) != 14) {
-        $mensagemErro .= "Preencha um CPF válido com exatamente 11 dígitos numéricos.<br>";
+      $mensagemErro .= "Preencha um CPF válido com exatamente 11 dígitos numéricos.<br>";
+      $erro = true;
+  }
+  
+
+    // Validação da data de nascimento (pode ser mais rigorosa)
+    if (empty($data_nascimento) || !strtotime($data_nascimento)) {
+        $mensagemErro .= "Preencha uma data de nascimento válida no formato YYYY-MM-DD.<br>";
         $erro = true;
     }
 
-    // Validação da data de nascimento 
-    if (empty($data_nascimento) || !strtotime($data_nascimento)) {
-        $mensagemErro .= "Preencha uma data de nascimento válida no formato YYYY-MM-DD.<br>";
+    // Validação do gênero
+    if (empty($genero)) {
+        $mensagemErro .= "Selecione seu gênero.<br>";
         $erro = true;
     }
 
@@ -63,8 +71,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
     
         // Usar consultas preparadas para evitar injeção de SQL
-        $stmt = $mysqli->prepare("INSERT INTO cadastrados (nome, email, senha, cpf, data_nascimento) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $nome, $email, $senhaHash, $cpf, $data_nascimento);
+        $stmt = $mysqli->prepare("INSERT INTO cadastrados (nome, email, senha, cpf, data_nascimento, genero) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $nome, $email, $senhaHash, $cpf, $data_nascimento, $genero);
     
         if ($stmt->execute()) {
             $mysqli->close();
@@ -81,253 +89,328 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Webleb</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
-    integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="/css/styles.css">
-  <style>
-    
-    @import url(https://fonts.googleapis.com/css?family=Poppins:300);
-    *,
-    *:before,
-    *:after {
-      box-sizing: border-box;
-    }
-    body {
-      height: 100vh;
-      background: #01939c;
-      overflow-y: hidden!important;
-      font-family: 'Poppins', sans-serif;
-    }
-    a {
-      text-decoration: none;
-      color: #01939c;
-      transition: 0.5s ease;
-    }
-    a:hover {
-      color: #179b77;
-    }
-    .form {
-      background: #12141d;
-      padding: 40px;
-      max-width: 600px;
-      margin: 40px auto;
-      border-radius: 15px;
-      box-shadow: 0 4px 10px 4px rgba(19, 35, 47, .3);
-    }
-    .tab-group {
-      list-style: none;
-      padding: 0;
-      margin: 0 0 40px 0;
-    }
-    .tab-group:after {
-      content: "";
-      display: table;
-      clear: both;
-    }
-    .tab-group li a {
-      display: block;
-      text-decoration: none;
-      padding: 15px;
-      background: rgba(160, 179, 176, .25);
-      color: #a0b3b0;
-      font-size: 20px;
-      float: left;
-      width: 48%;
-      text-align: center;
-      cursor: pointer;
-      transition: 0.5s ease;
-    }
-    .tab-group li a:hover {
-      background: #01939c;
-      color: #fff;
-    }
-    .tab-group .active a {
-      background: #01939c;
-      color: #fff;
-    }
-    .tab-content > div:last-child {
-      display: none;
-    }
-    .tab-group {
-      border-radius: 15px!important;
-      margin: 20px;
-    }
-    h1 {
-      text-align: center;
-      color: #fff;
-      font-weight: 300;
-      margin: 0 0 40px;
-    }
-    input,
-    textarea {
-      font-size: 17px;
-      display: block;
-      width: 100%;
-      height: 100%;
-      padding: 5px 10px;
-      background: none;
-      background-image: none;
-      border: 1px solid #01939c;
-      color: #fff;
-      border-radius: 6px;
-      transition: border-color 0.25s ease, box-shadow 0.25s ease;
-    }
-    textarea {
-      border: 2px solid #01939c;
-      resize: vertical;
-    }
-    .field-wrap {
-      position: relative;
-      margin-bottom: 40px;
-    }
-    .top-row:after {
-      content: "";
-      display: table;
-      clear: both;
-    }
-    .top-row > div {
-      float: left;
-      width: 48%;
-      margin-right: 4%;
-    }
-    .top-row > div:last-child {
-      margin: 0;
-    }
-    .button {
-      border: 0;
-      outline: none;
-      border-radius: 15px;
-      padding: 15px 0;
-      font-size: 20px;
-      font-weight: 400;
-      letter-spacing: 0.1em;
-      background: #01939c;
-      cursor: pointer;
-      color: #fff;
-      transition: all 0.5s ease;
-      -webkit-appearance: none;
-    }
-    .button:hover,
-    .button:focus {
-      background: #025c61;
-    }
-    .button-block {
-      display: block;
-      width: 100%;
-    }
-    .forgot {
-      margin-top: -20px;
-      text-align: right;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <style>
+          @import url('https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500&family=Open+Sans:wght@300;400;500;600&display=swap');
+      * {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+          font-family: 'Inter', sans-serif;
+      }
+
+      body {
+          width: 100%;
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background: #0c0ce94d;
+      }
+
+      .container {
+          width: 80%;
+          height: 80vh;
+          display: flex;
+          box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.212);
+      }
+
+      .form-image {
+          width: 50%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: #fde3a7d7;
+          padding: 1rem;
+      }
+
+      .form-image img {
+          width: 31rem;
+      }
+
+      .form {
+          width: 50%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          background-color: #fff;
+          padding: 3rem;
+      }
+
+      .form-header {
+          margin-bottom: 3rem;
+          display: flex;
+          justify-content: space-between;
+      }
+
+      .login-button {
+          display: flex;
+          align-items: center;
+      }
+
+      .login-button button {
+          border: none;
+          background-color: #6c63ff;
+          padding: 0.4rem 1rem;
+          border-radius: 5px;
+          cursor: pointer;
+      }
+
+      .login-button button:hover {
+          background-color: #6b63fff1;
+      }
+
+      .login-button button a {
+          text-decoration: none;
+          font-weight: 500;
+          color: #fff;
+      }
+
+      .form-header h1::after {
+          content: '';
+          display: block;
+          width: 5rem;
+          height: 0.3rem;
+          background-color: #6c63ff;
+          margin: 0 auto;
+          position: absolute;
+          border-radius: 10px;
+      }
+
+      .input-group {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          padding: 1rem 0;
+      }
+
+      .input-box {
+          display: flex;
+          flex-direction: column;
+          margin-bottom: 1.1rem;
+      }
+
+      .input-box input {
+          margin: 0.6rem 0;
+          padding: 0.8rem 1.2rem;
+          border: none;
+          border-radius: 10px;
+          box-shadow: 1px 1px 6px #0000001c;
+          font-size: 0.8rem;
+      }
+
+      .input-box input:hover {
+          background-color: #eeeeee75;
+      }
+
+      .input-box input:focus-visible {
+          outline: 1px solid #6c63ff;
+      }
+
+      .input-box label,
+      .gender-title h6 {
+          font-size: 0.75rem;
+          font-weight: 600;
+          color: #000000c0;
+      }
+
+      .input-box input::placeholder {
+          color: #000000be;
+      }
+
+      .gender-group {
+          display: flex;
+          justify-content: space-between;
+          margin-top: 0.62rem;
+          padding: 0 .5rem;
+      }
+
+      .gender-input {
+          display: flex;
+          align-items: center;
+      }
+
+      .gender-input input {
+          margin-right: 0.35rem;
+      }
+
+      .gender-input label {
+          font-size: 0.81rem;
+          font-weight: 600;
+          color: #000000c0;
+      }
+
+      .continue-button button {
+          width: 100%;
+          margin-top: 2.5rem;
+          border: none;
+          background-color: #6c63ff;
+          padding: 0.62rem;
+          border-radius: 5px;
+          cursor: pointer;
+      }
+
+      .continue-button button:hover {
+          background-color: #6b63fff1;
+      }
+
+      .continue-button button a {
+          text-decoration: none;
+          font-size: 0.93rem;
+          font-weight: 500;
+          color: #fff;
+      }
+
+      @media screen and (max-width: 1330px) {
+          .form-image {
+              display: none;
+          }
+          .container {
+              width: 50%;
+          }
+          .form {
+              width: 100%;
+          }
+      }
+
+      @media screen and (max-width: 1064px) {
+          .container {
+              width: 90%;
+              height: auto;
+          }
+          .input-group {
+              flex-direction: column;
+              z-index: 5;
+              padding-right: 5rem;
+              max-height: 10rem;
+              overflow-y: scroll;
+              flex-wrap: nowrap;
+          }
+          .gender-inputs {
+              margin-top: 2rem;
+          }
+          .gender-group {
+              flex-direction: column;
+          }
+          .gender-title h6 {
+              margin: 0;
+          }
+          .gender-input {
+              margin-top: 0.5rem;
+          }
+      }
+    </style>
+    <title>Formulário</title>
 </head>
+
 <body>
-  <div class="form">
-    <ul class="tab-group">
-      <li class="tab active"><a href="#signup" style="border-radius: 15px!important;margin-right:8x;">Sign Up</a></li>
-      <li class="tab"><a href="#login" style="border-radius: 15px!important;margin-left:8px;">Log In</a></li>
-    </ul>
-    <div class="tab-content">
-      <div id="signup">
-        <h1>Register</h1>
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-          <div class="field-wrap">
-            <input type="text" required placeholder="First Name" name="nome">
-          </div>
-          <div class="field-wrap">
-            <input type="email" required placeholder="Email Address" name="email">
-          </div>
-          <div class="field-wrap">
-            <input type="password" required placeholder="Password" name="senha">
-          </div>
-          <div class="field-wrap">
-            <input type="text" required placeholder="CPF" name="cpf" maxlength="14" oninput="formatarCPF(this)">
-          </div>
-          <div class="field-wrap">
-            <input type="date" required placeholder="Date of Birth" name="data_nascimento">
-          </div>
-          <button type="submit" class="button button-block">Sign Up</button>
-        </form>
-      </div>
-      <div id="login">
-        <h1>Welcome Back!</h1>
-        <form action="processa_login.php" method="post"> 
-          <div class="field-wrap">
-            <input type="email" required placeholder="Email" name="login_email">
-          </div>
-          <div class="field-wrap">
-            <input type="password" required placeholder="Password" name="login_senha">
-          </div>
-          <p class="forgot"><a href="https://www.web-leb.com/code">Forgot Password?</a></p>
-          <button type="submit" class="button button-block" name="login_submit">Login</button>
-        </form>
-      </div>
-    </div>
-  </div>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="/js/main.js"></script>
+    <div class="container">
+        <div class="form-image">
+            <img src="undraw_shopping_re_3wst.svg" alt="">
+        </div>
+        <div class="form">
+            <form action="#" method="post">
+                <div class="form-header">
+                    <div class="title">
+                        <h1>Cadastre-se</h1>
+                    </div>
+                    <div class="login-button">
+                        <button><a href="#">Entrar</a></button>
+                    </div>
+                </div>
 
-  <script>
-    function formatarCPF(campo) {
-      campo.value = campo.value.replace(/\D/g, ''); // Remove tudo que não é dígito
-      campo.value = campo.value.replace(/(\d{3})(\d)/, '$1.$2'); // Coloca o ponto após o terceiro dígito
-      campo.value = campo.value.replace(/(\d{3})(\d)/, '$1.$2'); // Coloca o segundo ponto após o sexto dígito
-      campo.value = campo.value.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // Coloca o traço após o nono dígito
-    }
-    $('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
-    var $this = $(this),
-        label = $this.prev('label');
+                <div class="input-group">
+                    <div class="input-box">
+                        <label for="firstname">Primeiro Nome</label>
+                        <input id="firstname" type="text" name="firstname" placeholder="Digite seu primeiro nome" required>
+                    </div>
 
-        if (e.type === 'keyup') {
-                if ($this.val() === '') {
-            label.removeClass('active highlight');
-            } else {
-            label.addClass('active highlight');
-            }
-        } else if (e.type === 'blur') {
-            if( $this.val() === '' ) {
-                label.removeClass('active highlight'); 
-                } else {
-                label.removeClass('highlight');   
-                }   
-        } else if (e.type === 'focus') {
-        
-        if( $this.val() === '' ) {
-                label.removeClass('highlight'); 
-                } 
-        else if( $this.val() !== '' ) {
-                label.addClass('highlight');
+                    <div class="input-box">
+                        <label for="lastname">Sobrenome</label>
+                        <input id="lastname" type="text" name="lastname" placeholder="Digite seu sobrenome" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="email">E-mail</label>
+                        <input id="email" type="email" name="email" placeholder="Digite seu e-mail" required>
+                    </div>
+
+                    <div class="input-box">
+                        <label for="cpf">CPF</label>
+                        <input id="cpf" type="text" name="cpf" placeholder="000.000.000-00" 
+                              oninput="formatarCPF(this);" maxlength="14" required>
+                    </div>
+
+
+
+                    <div class="input-box">
+                        <label for="password">Senha</label>
+                        <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
+                    </div>
+
+
+                    <div class="input-box">
+                        <label for="dataNascimento">Data de Nascimento</label>
+                        <input id="dataNascimento" type="date" name="dataNascimento" required>
+                    </div>
+
+                </div>
+
+                <div class="gender-inputs">
+                    <div class="gender-title">
+                        <h6>Gênero</h6>
+                    </div>
+
+                    <div class="gender-group">
+                        <div class="gender-input">
+                            <input id="female" type="radio" name="gender" value="Feminino">
+                            <label for="female">Feminino</label>
+                        </div>
+
+                        <div class="gender-input">
+                            <input id="male" type="radio" name="gender" value="Masculino">
+                            <label for="male">Masculino</label>
+                        </div>
+
+                        <div class="gender-input">
+                            <input id="others" type="radio" name="gender" value="Outros">
+                            <label for="others">Outros</label>
+                        </div>
+
+                        <div class="gender-input">
+                            <input id="none" type="radio" name="gender" value="Prefiro não dizer">
+                            <label for="none">Prefiro não dizer</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="continue-button">
+                    <button type="submit">Continuar</button>
+                </div>
+            </form>
+            <script>
+                function formatarCPF(campo) {
+                    campo.value = campo.value.replace(/\D/g, ''); // Remove tudo que não é dígito
+                    campo.value = campo.value.replace(/(\d{3})(\d)/, '$1.$2'); // Coloca o ponto após o terceiro dígito
+                    campo.value = campo.value.replace(/(\d{3})(\d)/, '$1.$2'); // Coloca o segundo ponto após o sexto dígito
+                    campo.value = campo.value.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // Coloca o traço após o nono dígito
                 }
-        }
-
-    });
-
-    $('.tab a').on('click', function (e) {
-    
-    e.preventDefault();
-    
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-    
-    target = $(this).attr('href');
-
-    $('.tab-content > div').not(target).hide();
-    
-    $(target).fadeIn(600);
-    
-    });
-
-  </script>
+            </script>
+        </div>
+    </div>
 </body>
+
 </html>
+
+
+
 
 
