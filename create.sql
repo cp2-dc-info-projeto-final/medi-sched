14f5691da495a854db3531192497aba442544a22
PedroHenriqueNM <136659414+PedroHenriqueNM@users.noreply.github.com>
2023-09-28 14:00:12 -0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -9,8 +9,8 @@ DROP TABLE IF EXISTS Cliente;
 
 CREATE TABLE IF NOT EXISTS Cliente (
     idUsuario INT AUTO_INCREMENT PRIMARY KEY,
-    primeiro_nome VARCHAR(100) NOT NULL,
-    sobrenome VARCHAR(100) NOT NULL,
+    nome_cliente VARCHAR(100) NOT NULL,
+    sobrenome_cliente VARCHAR(100) NOT NULL,
     email VARCHAR(200) NOT NULL,
     cpf VARCHAR(14) NOT NULL,
     senha VARCHAR(200) NOT NULL,
@@ -23,11 +23,12 @@ DROP TABLE IF EXISTS Funcionario;
 
 CREATE TABLE Funcionario (
     idFuncionario int NOT NULL AUTO_INCREMENT,
-    nome_funcionario varchar(200) NOT NULL,
+    nome_funcionario varchar(100) NOT NULL,
+    sobrenome_funcionario varchar(100) NOT NULL,
     email varchar(200) NOT NULL,
     senha varchar(200) NOT NULL,
     data_nascimento DATE NOT NULL,
-    cpf varchar(11) NOT NULL,
+    cpf varchar(14) NOT NULL,
     cargo varchar(50) NOT NULL,
     Genero VARCHAR(50) NOT NULL,
 
@@ -39,8 +40,10 @@ DROP TABLE IF EXISTS Agendamento;
 CREATE TABLE Agendamento (
     idAgendamento int NOT NULL AUTO_INCREMENT,
     nome_servico varchar(200) NOT NULL FOREIGN KEY REFERENCES Servico(nome_servico),
-    nome_funcionario varchar(200) NOT NULL FOREIGN KEY REFERENCES Funcionario(nome_funcionario),
-    nome_cliente varchar(200) NOT NULL FOREIGN KEY REFERENCES Cliente(nome_cliente),
+    nome_funcionario varchar(100) NOT NULL FOREIGN KEY REFERENCES Funcionario(nome_funcionario),
+    sobrenome_funcionario varchar(100) NOT NULL FOREIGN KEY REFERENCES Funcionario(sobrenome_funcionario),
+    nome_cliente varchar(100) NOT NULL FOREIGN KEY REFERENCES Cliente(nome_cliente),
+    sobrenome_cliente varchar(100) NOT NULL FOREIGN KEY REFERENCES Cliente(sobrenome_cliente),
     data_consulta DATE NOT NULL,
     horario_consulta TIME NOT NULL,
 
