3b3492bb7b754c66f1f0c1c051add340fd9d0068
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-10-30 14:15:54 -0300

    Update and rename cadastro_func.php to funcionario.php

--- src/funcionario.php
+++ src/funcionario.php
@@ -1,7 +1,9 @@
 <?php
-$mensagemErro = ""; // Inicializa a mensagem de erro como uma string vazia
+
+$mensagemErro = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
     $primeiro_nome = $_POST["firstname"];
     $sobrenome = $_POST["lastname"];
     $email = $_POST["email"];
@@ -9,61 +11,71 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $cpf = $_POST["cpf"];
     $data_nascimento = $_POST["dataNascimento"];
     $genero = $_POST["gender"];
-    $cargo = $_POST["cargo"];
-    
-    $erro = false; // Inicializar a variável $erro
+    $cargo = $_POST["areasMedicas"]; 
 
-    // Validação do nome completo
+    $erro = false;
+
     if (empty($primeiro_nome) || empty($sobrenome)) {
-        $mensagemErro .= "Preencha seu nome completo com sobrenome.<br>";
+        $mensagemErro .= "Preencha o nome completo com sobrenome.<br>";
         $erro = true;
     }
 
-    // Validação do email
     if (strlen($email) < 8 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $mensagemErro .= "Preencha um email válido com pelo menos 8 caracteres.<br>";
         $erro = true;
     }
 
-    // Validação da senha
     if (strlen($senha) < 8) {
         $mensagemErro .= "A senha deve ter pelo menos 8 caracteres.<br>";
         $erro = true;
     }
 
-    // Validação do CPF
     if (empty($cpf) || strlen($cpf) != 14) {
         $mensagemErro .= "Preencha um CPF válido com exatamente 11 dígitos numéricos.<br>";
         $erro = true;
     }
-
-    // Validação da data de nascimento
+    
     if (empty($data_nascimento) || !strtotime($data_nascimento)) {
         $mensagemErro .= "Preencha uma data de nascimento válida no formato YYYY-MM-DD.<br>";
         $erro = true;
     }
 
-    // Validação do gênero
     if (empty($genero)) {
-        $mensagemErro .= "Selecione seu gênero.<br>";
+        $mensagemErro .= "Selecione o gênero.<br>";
         $erro = true;
     }
 
-    // Validação do cargo
-    if (empty($cargo)) {
-        $mensagemErro .= "Selecione seu cargo.<br>";
+    // Conectar ao banco de dados
+    $mysqli = new mysqli("localhost", "seu_usuario", "sua_senha", "seu_banco_de_dados");
+
+    if ($mysqli->connect_error) {
+        die("Erro na conexão: " . $mysqli->connect_error);
+    }
+
+    // Verificar se o email já está cadastrado
+    $stmt = $mysqli->prepare("SELECT email FROM Funcionario WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $mensagemErro .= "Este endereço de email já está cadastrado.<br>";
         $erro = true;
     }
 
-    if (!$erro) {
+    $stmt->close();
+    
+    if (!$erro) { 
+        // Criptografar a senha
         $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
     
-        $stmt = $mysqli->prepare("INSERT INTO funcionario (nome_funcionario, sobrenome_funcionario, email, senha, cpf, data_nascimento, cargo, genero) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssssss", $primeiro_nome, $sobrenome, $email, $senhaHash, $cpf, $data_nascimento, $genero);
+        // Inserir dados na tabela Funcionario
+        $stmt = $mysqli->prepare("INSERT INTO Funcionario (nome_funcionario, sobrenome_funcionario, email, senha, cpf, data_nascimento, cargo, genero) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssssss", $primeiro_nome, $sobrenome, $email, $senhaHash, $cpf, $data_nascimento, $cargo, $genero);
     
         if ($stmt->execute()) {
             $mysqli->close();
-            header("Location: index.html");
+            header("Location: index.html"); // Redireciona para a página inicial
             exit;
         } else {
             $mensagemErro .= "Erro ao inserir dados: " . $stmt->error;
@@ -73,36 +85,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     
     $mysqli->close();
-    }
-    
-?>
+}
 
+echo $mensagemErro; // Exibe as mensagens de erro no formulário
+
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style2.css">
-
+    <link rel="stylesheet" href="funcionario.css">
     <title>Formulário</title>
 </head>
-
 <body>
     <div class="container">
         <div class="form-image">
             <img src="med.png" alt="">
         </div>
         <div class="form">
-            <form action="cadastro_func" method="post">
+            <form action="cadastro.php" method="post">
                 <div class="form-header">
                     <div class="title">
                         <h1>Cadastre-se</h1>
                     </div>
                     <div class="login-button">
-                        <button><a href="#">Entrar</a></button>
+                        <button><a href="login.php">Entrar</a></button>
                     </div>
                 </div>
 
@@ -114,7 +124,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     <div class="input-box">
                         <label for="lastname">Sobrenome</label>
-                        <input id="lastname" type="text" name="lastname" placeholder="Digite seu sobrenome" required>
+                        <input id="lastname" type="text" name "lastname" placeholder="Digite seu sobrenome" required>
                     </div>
                     <div class="input-box">
                         <label for="email">E-mail</label>
@@ -123,35 +133,58 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     <div class="input-box">
                         <label for="cpf">CPF</label>
-                        <input id="cpf" type="text" name="cpf" placeholder="000.000.000-00" 
-                              oninput="formatarCPF(this);" maxlength="14" required>
+                        <input id="cpf" type="text" name="cpf" placeholder="000.000.000-00" oninput="formatarCPF(this);" maxlength="14" required>
                     </div>
-
-
 
                     <div class="input-box">
                         <label for="password">Senha</label>
                         <input id="password" type="password" name="password" placeholder="Digite sua senha" required>
                     </div>
 
-
                     <div class="input-box">
                         <label for="dataNascimento">Data de Nascimento</label>
                         <input id="dataNascimento" type="date" name="dataNascimento" required>
+                    </div>
+
+                    <div class="input-box">
+                        <label for="areasMedicas">Áreas Médicas</label>
+                        <select id="areasMedicas" name="areasMedicas" required>
+                            <option value="cardiologia">Cardiologia</option>
+                            <option value="ortopedia">Ortopedia</option>
+                            <option value="dermatologia">Dermatologia</option>
+                            <option value="ginecologia">Ginecologia</option>
+
+                        </select>
                     </div>
 
                 </div>
 
                 <div class="gender-inputs">
                     <div class="gender-title">
-                    <label for="cargo">Cargo:</label>
-                        <select id="cargo" name="cargo" required>
-                            <option value="Cirurgião">Cirurgião</option>
-                            <option value="Enfermeiro">Enfermeiro</option>
-                            <option value="Ortopedista">Ortopedista</option>
-                            <option value="Dermatologista">Dermatologista</option>
-                            <option value="Dentista">Dentista</option>
-                        </select>
+                        <h6>Gênero</h6>
+                    </div>
+
+                    <div class="gender-group">
+                        <div class="gender-input">
+                            <input id="female" type="radio" name="gender" value="Feminino">
+                            <label for="female">Feminino</label>
+                        </div>
+
+                        <div class="gender-input">
+                            <input id="male" type="radio" name="gender" value="Masculino">
+                            <label for="male">Masculino</label>
+                        </div>
+
+                        <div class="gender-input">
+                            <input id="others" type="radio" name="gender" value="Outros">
+                            <label for="others">Outros</label>
+                        </div>
+
+                        <div class="gender-input">
+                            <input id="none" type="radio" name="gender" value="Prefiro não dizer">
+                            <label for="none">Prefiro não dizer</label>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="continue-button">
@@ -169,5 +202,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
 </body>
-
 </html>
+
+
