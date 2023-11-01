29102d03f3a7b4fd7779c061e85fe0f8f9dc3f8f
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-10-31 21:34:45 -0300

    Update login.php

--- src/login.php
+++ src/login.php
@@ -36,7 +36,7 @@ mysqli_close($mysqli);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylese.css">
+    <link rel="stylesheet" href="login.css">
     <title>Login</title>
 </head>
 
