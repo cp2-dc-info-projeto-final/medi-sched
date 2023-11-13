a0e418ed1f24cf96658402a8bcadebc1d9471ec7
caiogabriel2 <142699957+caiogabriel2@users.noreply.github.com>
2023-11-13 13:49:08 -0300

    Update recuperar_cod.php

--- src/recuperar_cod.php
+++ src/recuperar_cod.php
@@ -6,9 +6,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<link rel="shortcut icon" href="_img/logo_petgato.png" type="image/x-icon" />
+	<link rel="shortcut icon" href=".img/logo.png" type="image/x-icon" />
 
-	<link rel="stylesheet" href="_css/style.css">
+	<link rel="stylesheet" href=".css">
 
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
