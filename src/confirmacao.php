ddf54890544467a6f869b55921d45a154f9c3bc5
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-17 17:57:24 -0300

    Create confirmacao.php

---
+++ src/confirmacao.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <title>Confirmação de Agendamento | Agenda+Saúde</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href=".img/logo.png" type="image/x-icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href=".css/agenda.css"/>
+</head>
+<body class="fadeIn">
+    <div class="container mt-5">
+        <div class="text-center">
+            <h2>Agendamento Confirmado!</h2>
+            <img src=".img/joinha.png" alt="Confirmação" style="width: 150px;" class="my-3">
+            <p>Seu agendamento foi realizado com sucesso.</p>
+            <p>Por favor, verifique seu e-mail para mais detalhes.</p>
+            <a href="index.php" class="btn btn-primary">Voltar para a Página Inicial</a>
+        </div>
+    </div>
+</body>
+</html>
