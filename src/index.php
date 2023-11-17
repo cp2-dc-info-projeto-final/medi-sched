f1745df9bc42581aec365971553581d0c245b9b5
PedroHenriqueNM <136659414+PedroHenriqueNM@users.noreply.github.com>
2023-11-17 10:22:04 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -157,7 +157,7 @@
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4" >Contato</h6>
-          <p> <img src=".img/home.png" class="img-fluid" ><i class="fas fa-home me-3"></i> Rua Manoel Reis, 15 - Duque de Caxias- Rj </p>
+          <p> <img src=".img/casa.png" class="img-fluid" ><i class="fas fa-casa me-3"></i> Rua Manoel Reis, 15 - Duque de Caxias- Rj </p>
           <p> <img src=".img/mail.png" class="img-fluid" >
             <i class="fas fa-envelope me-3"></i>
             agendasaude1@gmail.com
