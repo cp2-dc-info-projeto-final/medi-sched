234350c68a07245078c74aa522a5b8fb6baa55ba
PedroHenriqueNM <136659414+PedroHenriqueNM@users.noreply.github.com>
2023-11-22 15:14:24 -0300

    Update atendimentos.php

--- src/atendimentos.php
+++ src/atendimentos.php
@@ -175,7 +175,7 @@
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4" >Contato</h6>
-          <p> <img src=".img/home.png" class="img-fluid" ><i class="fas fa-home me-3"></i> Rua Manoel Gomes, 20 - Duque de Caxias- Rj </p>
+          <p> <img src=".img/casa.png" class="img-fluid" ><i class="fas fa-home me-3"></i> Rua Manoel Gomes, 20 - Duque de Caxias- Rj </p>
           <p> <img src=".img/mail.png" class="img-fluid" >
             <i class="fas fa-envelope me-3"></i>
             agendasaude1@gmail.com
