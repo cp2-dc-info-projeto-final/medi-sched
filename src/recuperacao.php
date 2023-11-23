fa2d50d48fb99558b1597ca4489ac4322bc3674c
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-22 23:35:32 -0300

    Update recuperacao.php

--- src/recuperacao.php
+++ src/recuperacao.php
@@ -1,6 +1,6 @@
 <?php
 include "envia_email.php";
-include "conecta_mysql.inc"; // Se você precisar conectar-se ao banco de dados
+include "conecta_mysql.php"; // Se você precisar conectar-se ao banco de dados
 
 session_start();
 
