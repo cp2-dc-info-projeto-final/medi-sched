68b8510b6859bca27b297e2d89dad2c06ff47aca
PedroHenriqueNM <136659414+PedroHenriqueNM@users.noreply.github.com>
2023-11-23 17:23:06 -0300

    Update agendamento.php

--- src/agendamento.php
+++ src/agendamento.php
@@ -2,6 +2,7 @@
 $mensagemErro = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Dados do agendamento
     $idServico = $_POST["idServico"];
     $idFuncionario = $_POST["idFuncionario"];
     $idCliente = $_POST["idCliente"];
@@ -10,32 +11,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $erro = false;
 
+    // Validações básicas
     if (empty($idServico) || empty($idFuncionario) || empty($idCliente)) {
         $mensagemErro .= "Todos os campos do agendamento são obrigatórios.<br>";
         $erro = true;
     }
 
     if (empty($dataConsulta) || !strtotime($dataConsulta)) {
-        $mensagemErro .= "Preencha uma data de consulta válida.<br>";
+        $mensagemErro .= "Preencha uma data de consulta válida no formato DD-MM-YYYY.<br>";
         $erro = true;
     }
 
-    if (empty($horarioConsulta) || !preg_match("/^(10|11|12|13|14|15|16|17|18):[0-5][0-9]$/", $horarioConsulta)) {
-        $mensagemErro .= "Preencha um horário de consulta válido entre 10:00 e 18:00.<br>";
+    if (empty($horarioConsulta) || !preg_match("/^[0-2][0-9]:[0-5][0-9]$/", $horarioConsulta)) {
+        $mensagemErro .= "Preencha um horário de consulta válido .<br>";
         $erro = true;
     }
 
+    // Cria a conexão com o banco de dados
     $mysqli = new mysqli("localhost", "agendasaude", "123", "AGENDASAUDE");
     if ($mysqli->connect_error) {
         die("Erro na conexão: " . $mysqli->connect_error);
     }
 
+    // Verifica se já existe um agendamento para o horário selecionado
+    $stmt = $mysqli->prepare("SELECT idAgendamento FROM Agendamento WHERE idFuncionario = ? AND data_consulta = ? AND horario_consulta = ?");
+    $stmt->bind_param("iss", $idFuncionario, $dataConsulta, $horarioConsulta);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $mensagemErro .= "Já existe um agendamento para este horário com o funcionário selecionado.<br>";
+        $erro = true;
+    }
+
+    $stmt->close();
+
     if (!$erro) {
+        // Insere o novo agendamento no banco de dados
         $stmt = $mysqli->prepare("INSERT INTO Agendamento (idServico, idFuncionario, idCliente, data_consulta, horario_consulta) VALUES (?, ?, ?, ?, ?)");
         $stmt->bind_param("iiiss", $idServico, $idFuncionario, $idCliente, $dataConsulta, $horarioConsulta);
 
         if ($stmt->execute()) {
-            header("Location: confirmacao.php");
+            // Redireciona para uma página de confirmação ou outra página conforme necessário
+            $stmt->close();
+            $mysqli->close();
+            header("Location: .php"); // Não fiz a pagina de confirmação ainda
             exit;
         } else {
             $mensagemErro .= "Erro ao inserir o agendamento: " . $stmt->error;
@@ -47,6 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mysqli->close();
 }
 
+// Se houver mensagens de erro, elas serão exibidas na página
 echo $mensagemErro;
 ?>
 
@@ -66,7 +87,7 @@ echo $mensagemErro;
     <div id="header">
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
-                <a class="navbar-brand" href="index.php"><img src=".img/logo.png" class="img-center" width="45%"/></a>
+                <a class="navbar-brand" href="index.php"><img src=".img/logo.png" class="img-center" width="25%"/></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -81,16 +102,12 @@ echo $mensagemErro;
                         <li class="nav-item">
                             <a class="nav-link" href="#rodape">Sobre</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="meus_agend.php">Meus Agendamentos</a>
-                        </li>
-                        
                     </ul>
                 </div>
             </nav>
         </div>
     </div>
-    
+
 <div class="container mt-5">
     <h2 class="mb-4">Agendar Consulta</h2>
     <!-- Mensagem de erro, se houver -->
@@ -105,12 +122,14 @@ echo $mensagemErro;
             <label for="idServico" class="form-label">Serviço</label>
             <select class="form-select" id="idServico" name="idServico">
                 <option selected>Escolha um serviço...</option>
-                <option value="1">Ortopedia</option>
+                <option value="1">Consulta Geral</option>
                 <option value="2">Vacinação</option>
-                <option value="3">Oftalmologia</option>
-                <option value="4">Odontologico</option>
-                <option value="5">Psicologo</option>
-                <option value="6">Ginecologia</option>
+                <option value="3">Check-up Anual</option>
+                <option value="4">Gerenciamento de Doenças Crônicas</option>
+                <option value="5">Serviços de Saúde Mental</option>
+                <option value="6">Saúde da Mulher</option>
+                <option value="7">Atendimento de Urgência</option>
+                <option value="8">Exame de Rotina</option>
             </select>
         </div>
         
@@ -123,9 +142,11 @@ echo $mensagemErro;
                 
             </select>
         </div>
+        
         <div class="mb-3">
-            <label for="idCliente" class="form-label">ID Cliente</label>
-            <input type="text" class="form-control" id="idCliente" name="idCliente" placeholder="Digite seu id">
+            <label for="idCliente" class="form-label">Cliente</label>
+            <input type="text" class="form-control" id="idCliente" name="idCliente" placeholder="Seu ID de cliente">
+        </div>
         
         <div class="mb-3">
             <label for="dataConsulta" class="form-label">Data da Consulta</label>
@@ -134,19 +155,8 @@ echo $mensagemErro;
         
         <div class="mb-3">
             <label for="horarioConsulta" class="form-label">Horário da Consulta</label>
-            <select class="form-control" id="horarioConsulta" name="horarioConsulta">
-                <option value="10:00">10:00</option>
-                <option value="11:00">11:00</option>
-                <option value="12:00">12:00</option>
-                <option value="13:00">13:00</option>
-                <option value="14:00">14:00</option>
-                <option value="15:00">15:00</option>
-                <option value="16:00">16:00</option>
-                <option value="17:00">17:00</option>
-                <option value="18:00">18:00</option>
-            </select>
+            <input type="time" class="form-control" id="horarioConsulta" name="horarioConsulta">
         </div>
-
         
         <button type="submit" class="btn btn-primary">Agendar</button>
     </form>
@@ -154,4 +164,3 @@ echo $mensagemErro;
 
 </body>
 </html>
- 
