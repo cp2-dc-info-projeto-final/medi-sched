d448080e859c8385869bd3e0742426bc17fca678
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-23 13:21:24 -0300

    Update index_paciente.php

--- src/index_paciente.php
+++ src/index_paciente.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="perfil.php">Perfil</a>
                         </li>
+			<li class="nav-item">
+                	    <a class="nav-link" href="logout.php">Logout</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
