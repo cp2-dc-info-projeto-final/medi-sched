903eba3a1d73e9e6a16e616d5f7c6dd317daf797
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-24 10:28:11 -0300

    Update perfil.php

--- src/perfil.php
+++ src/perfil.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>MediShed</title>
+    <title>Agenda+Saúde</title>
     <link rel="stylesheet" href="" />
   </head>
 
