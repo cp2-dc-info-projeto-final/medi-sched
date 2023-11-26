aaf42a0d64e50aadc4f0689cc70a52eb391d2b17
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-26 00:42:15 -0300

    Update index_paciente.php

--- src/index_paciente.php
+++ src/index_paciente.php
@@ -59,24 +59,24 @@
     </div>
   </div>
   <section class="ftco-section ftco-no-pt ftco-no-pb">
-    	<div class="container">
-    		<div class="row d-flex no-gutters">
-    			<div class="col-md-5 d-flex">
-    				<div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-center mb-4 mb-sm-0">
-                        <img src=".img/medico.png" class="img-fluid"  height= "150" width="100%"alt="Imagem simbolizando a saúde." >
-    				</div>
-    			</div>
-    			<div class="col-md-7 pl-md-5 py-md-5">
-    				<div class="heading-section pt-md-5">
-	            <h2 class="mb-4">Nossas especialidades:</h2>
-    				</div>
-    				<div class="row">
-	    				<div class="col-md-6 services-2 w-100 d-flex">
-	    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-stethoscope"></span></div>
-	    					<div class="text pl-3">
-	    						<h4>Ortopedia</h4>
-	    						<p>A ortopedia trata de lesões e doenças do sistema musculoesquelético, oferecendo cuidados especializados para ossos, articulações, ligamentos, tendões e músculos, garantindo movimento e atividade plena.</p>
-	    					</div>
+    <div class="container">
+    	<div class="row d-flex no-gutters">
+    		<div class="col-md-5 d-flex">
+    		<div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-center mb-4 mb-sm-0">
+                	<img src=".img/medico.png" class="img-fluid"  height= "150" width="100%"alt="Imagem simbolizando a saúde." >
+    		</div>
+    		</div>
+    		<div class="col-md-7 pl-md-5 py-md-5">
+    			<div class="heading-section pt-md-5">
+	            	<h2 class="mb-4">Nossas especialidades:</h2>
+    		</div>
+    		<div class="row">
+	    	<div class="col-md-6 services-2 w-100 d-flex">
+	    	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-stethoscope"></span></div>
+	    		<div class="text pl-3">
+	    			<h4>Ortopedia</h4>
+	    				<p>A ortopedia trata de lesões e doenças do sistema musculoesquelético, oferecendo cuidados especializados para ossos, articulações, ligamentos, tendões e músculos, garantindo movimento e atividade plena.</p>
+	    			</div>
 	    				</div>
 	    				<div class="col-md-6 services-2 w-100 d-flex">
 	    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-medical"></span></div>
@@ -100,8 +100,8 @@
 	    					</div>
 	    				</div>
 	    			</div>
-	        </div>
-        </div>
+			</div>
+        	</div>
     	</div>
     </section>
 
