56bb4325380c1404b92b2214fa89dea36308ff09
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-26 16:28:10 -0300

    Update meus_agend.php

--- src/meus_agend.php
+++ src/meus_agend.php
@@ -1,54 +1,22 @@
 <?php
 session_start();
-include "conecta_mysql.php"; 
 
-if (!isset($_SESSION['email'])) {
-    header('Location: login.php');
+// Certifique-se de que uma mensagem de status foi definida
+if (!isset($_SESSION['mensagemStatus'])) {
+    // Se não houver uma mensagem de status, redirecione para a página principal
+    header('Location: index_paciente.php');
     exit;
 }
 
-$email = $_SESSION['email'];
-$agendamentos = array();
-
-if ($mysqli->connect_error) {
-    die("Erro na conexão: " . $mysqli->connect_error);
-}
-
-// Obtem o idCliente com base no email do cliente logado
-$stmt = $mysqli->prepare("SELECT idCliente FROM Cliente WHERE email = ?");
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
-$idCliente = null;
-if ($row = $result->fetch_assoc()) {
-    $idCliente = $row['idCliente'];
-}
-$stmt->close();
-
-// Se o idCliente foi encontrado, busca os agendamentos
-if ($idCliente !== null) {
-    $stmt = $mysqli->prepare("SELECT idAgendamento, idServico, idFuncionario, data_consulta, horario_consulta FROM Agendamento WHERE idCliente = ?");
-    $stmt->bind_param("i", $idCliente);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $agendamentos[] = $row;
-        }
-    } else {
-        echo "<p>Você não tem agendamentos marcados.</p>";
-    }
-    $stmt->close();
-}
-
-$mysqli->close();
+// Armazena a mensagem de status e limpa-a da sessão
+$mensagemStatus = $_SESSION['mensagemStatus'];
+unset($_SESSION['mensagemStatus']);
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <title>Meus Agendamentos | Agenda+Saúde</title>
+    <title>Status do Agendamento | Agenda+Saúde</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href=".img/logo.png" type="image/x-icon" />
@@ -58,39 +26,13 @@ $mysqli->close();
 </head>
 <body>
     <div class="container mt-5">
-        <h2>Meus Agendamentos</h2>
-        <?php if (count($agendamentos) > 0): ?>
-            <table class="table table-striped mt-4">
-                <thead>
-                    <tr>
-                        <th>ID do Agendamento</th>
-                        <th>ID do Serviço</th>
-                        <th>ID do Funcionário</th>
-                        <th>ID do Cliente</th>
-                        <th>Data da Consulta</th>
-                        <th>Horário da Consulta</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($agendamentos as $agendamento): ?>
-                        <tr>
-                            <td><?php echo htmlspecialchars($agendamento['idAgendamento']); ?></td>
-                            <td><?php echo htmlspecialchars($agendamento['idServico']); ?></td>
-                            <td><?php echo htmlspecialchars($agendamento['idFuncionario']); ?></td>
-                            <td><?php echo isset($agendamento['idCliente']) ? htmlspecialchars($agendamento['idCliente']) : 'Não informado'; ?></td>
-                            <td><?php echo htmlspecialchars($agendamento['data_consulta']); ?></td>
-                            <td><?php echo htmlspecialchars($agendamento['horario_consulta']); ?></td>
-                            <td>
-                                <a href="editar_agendamento.php?id=<?php echo $agendamento['idAgendamento']; ?>" class="btn btn-primary btn-sm">Editar</a>
-                                <a href="cancela_agendamento.php?id=<?php echo $agendamento['idAgendamento']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Tem certeza que deseja cancelar este agendamento?');">Cancelar</a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        <?php else: ?>
-            <p>Você não tem agendamentos marcados.</p>
-        <?php endif; ?>
+        <h2>Status do Agendamento</h2>
+        <div class="alert alert-info" role="alert">
+            <?php echo htmlspecialchars($mensagemStatus); ?>
+        </div>
+        <div class="mt-3">
+            <a href="index_paciente.php" class="btn btn-primary">Voltar para a Página Inicial</a>
+        </div>
     </div>
 </body>
 </html>
