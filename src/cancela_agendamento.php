4acf2b90aae0ed7c159f342142872a2dfc3f4c6c
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-27 10:55:59 -0300

    Update cancela_agendamento.php

--- src/cancela_agendamento.php
+++ src/cancela_agendamento.php
@@ -38,7 +38,6 @@ exit;
     <title>Cancelar Agendamento | Agenda+Saúde</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
 </head>
 <body>
     <div class="container mt-5">
