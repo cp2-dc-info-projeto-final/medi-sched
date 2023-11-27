dbafff0c6e88725d50de4a5ea7d274c81fb7cfb1
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-27 13:58:42 -0300

    Update recuperacao.php

--- src/recuperacao.php
+++ src/recuperacao.php
@@ -4,8 +4,6 @@ include "conecta_mysql.php"; // Se você precisar conectar-se ao banco de dados
 
 session_start();
 
-$nome = $_POST['nome']; 
-$data = date("d/m/Y"); 
 $email = $_POST['email']; 
 
 
@@ -14,8 +12,8 @@ $recuperacao_codigo = rand(100000, 999999);
 $assunto = "Recuperação de Senha";
 
 $mensagem = "Dados para recuperação de senha:<br>";
-$mensagem .= "<br><b>Nome:</b> $nome";
-$mensagem .= "<br><b>Data:</b> $data";
+$mensagem .= "<br><b>Nome:</b>";
+$mensagem .= "<br><b>Data:</b>";
 $mensagem .= "<br><b>E-mail:</b> $email";
 $mensagem .= "<br><b>Código de Recuperação:</b> $recuperacao_codigo";
 $mensagem .= "<br><br>Clique no link abaixo para redefinir sua senha:";
@@ -23,7 +21,7 @@ $mensagem .= "<br><a href='http://seusite.com/redefinir_senha.php?codigo=$recupe
 
 if(envia_email($email, $assunto, $mensagem)){
     $_SESSION['msg_rec'] = "Instruções de recuperação de senha foram enviadas para o seu e-mail.";
-    header("Location: verifica_codigo.php"); // Redireciona para a página inicial de recuperação
+    header("Location: recuperar_cod.php"); // Redireciona para a página inicial de recuperação
 } else {
     $_SESSION['msg_rec'] = "Falha no envio do e-mail. Por favor, tente novamente.";
     header("Location: recuperar_senha.php"); // Redireciona para a página inicial de recuperação
