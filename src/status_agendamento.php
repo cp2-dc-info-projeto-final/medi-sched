42b635f078943d01b5e963baa898d3ee84dabd13
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-27 11:00:38 -0300

    Update status_agendamento.php

--- src/status_agendamento.php
+++ src/status_agendamento.php
@@ -1,34 +1,38 @@
 <?php
 session_start();
+
+// Certifique-se de que uma mensagem de status foi definida
+if (!isset($_SESSION['mensagemStatus'])) {
+    // Se não houver uma mensagem de status, redirecione para a página principal
+    header('Location: index_paciente.php');
+    exit;
+}
+
+// Armazena a mensagem de status e limpa-a da sessão
+$mensagemStatus = $_SESSION['mensagemStatus'];
+unset($_SESSION['mensagemStatus']);
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
     <title>Status do Agendamento | Agenda+Saúde</title>
-    <link rel="stylesheet" href=".css/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href=".img/logo.png" type="image/x-icon" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css"/>
 </head>
 <body>
     <div class="container mt-5">
         <h2>Status do Agendamento</h2>
-        
-        <!-- Verifica se existe alguma mensagem na sessão -->
-        <?php if(isset($_SESSION['mensagem'])): ?>
-            <div class="alert alert-info">
-                <?php 
-                echo $_SESSION['mensagem']; 
-                // Limpar a mensagem após exibição
-                unset($_SESSION['mensagem']);
-                ?>
-            </div>
-        <?php endif; ?>
-        
-        <!-- Botões de ação -->
-        <a href="meus_agend.php" class="btn btn-primary">Voltar aos Meus Agendamentos</a>
-        <a href="index_paciente.php" class="btn btn-secondary">Voltar ao Início</a>
+        <div class="alert alert-info" role="alert">
+            <?php echo htmlspecialchars($mensagemStatus); ?>
+        </div>
+        <div class="mt-3">
+            <a href="index_paciente.php" class="btn btn-primary">Voltar para a Página Inicial</a>
+        </div>
     </div>
 </body>
 </html>
