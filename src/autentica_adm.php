3be152ad97a02b6024fe4f0248ada9b075fb2bce
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-11-30 00:57:35 -0300

    Update autentica_adm.php

--- src/autentica_adm.php
+++ src/autentica_adm.php
@@ -4,11 +4,29 @@ session_start();
 // Inclui o arquivo de conexão com o banco de dados
 include "conecta_mysql.php";
 
-// Verifica se o usuário está logado e se é um administrador
-if (!isset($_SESSION['tipo_usuario']) || $_SESSION['tipo_usuario'] !== 'administrador') {
-    // Usuário não está logado ou não é um administrador
-    // Redireciona para a página de login
+// Verifica se o usuário está logado
+if (!isset($_SESSION['email']) || !isset($_SESSION['tipo_usuario'])) {
+    $_SESSION['erro_login'] = "Você precisa fazer login para acessar esta página.";
     header("Location: login.php");
-    exit; // Impede a execução do restante do script
+    exit;
 }
+
+$email = $_SESSION['email'];
+$tipo_usuario = $_SESSION['tipo_usuario'];
+
+// Verifica se o usuário é um administrador no banco de dados
+$stmt = $mysqli->prepare("SELECT * FROM Administrador WHERE email = ?");
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+
+// Se não encontrar o usuário ou se o tipo na sessão não for 'administrador', redireciona
+if ($result->num_rows != 1 || $tipo_usuario !== 'administrador') {
+    $_SESSION['erro_login'] = "Acesso restrito a administradores.";
+    header("Location: login.php");
+    exit;
+}
+
+// Fecha a conexão com o banco de dados
+mysqli_close($mysqli);
 ?>
