e919cfa97600be7ff95d7bd7f87be63ba556b03d
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-12-01 00:43:01 -0300

    Update recuperacao.php

--- src/recuperacao.php
+++ src/recuperacao.php
@@ -6,8 +6,16 @@ session_start();
 
 $email = $_POST['email']; 
 
+// Gerar uma senha aleatória de 6 dígitos
+$recuperacao_senha = rand(100000, 999999);
 
-$recuperacao_codigo = rand(100000, 999999);
+// Criptografar a senha antes de armazená-la no banco de dados
+$senha_criptografada = password_hash($recuperacao_senha, PASSWORD_DEFAULT);
+
+// Atualizar a senha no banco de dados para o usuário com o email fornecido
+$sql = "UPDATE Cliente SET senha = ? WHERE email = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$senha_criptografada, $email]);
 
 $assunto = "Recuperação de Senha";
 
@@ -15,9 +23,7 @@ $mensagem = "Dados para recuperação de senha:<br>";
 $mensagem .= "<br><b>Nome:</b>";
 $mensagem .= "<br><b>Data:</b>";
 $mensagem .= "<br><b>E-mail:</b> $email";
-$mensagem .= "<br><b>Código de Recuperação:</b> $recuperacao_codigo";
-$mensagem .= "<br><br>Clique no link abaixo para redefinir sua senha:";
-$mensagem .= "<br><a href='http://seusite.com/redefinir_senha.php?codigo=$recuperacao_codigo'>Redefinir Senha</a>";
+$mensagem .= "<br><b>Nova Senha:</b> $recuperacao_senha"; // Mostrar a nova senha gerada
 
 if(envia_email($email, $assunto, $mensagem)){
     $_SESSION['msg_rec'] = "Instruções de recuperação de senha foram enviadas para o seu e-mail.";
@@ -28,3 +34,4 @@ if(envia_email($email, $assunto, $mensagem)){
 }
 exit;
 ?>
+
