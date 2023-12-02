b5c200190d8fa99475788e3fd766f20d8bb63625
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-12-02 02:26:08 -0300

    Create cancela_agend_func.php

---
+++ src/cancela_agend_func.php
@@ -0,0 +1,30 @@
+<?php
+include "conecta_mysql.php";
+
+if (isset($_GET['idAgendamento'])) {
+    $idAgendamento = $_GET['idAgendamento'];
+
+    // Prepara a declaração para cancelar o agendamento
+    if ($stmt = $mysqli->prepare("DELETE FROM Agendamento WHERE idAgendamento = ?")) {
+        $stmt->bind_param("i", $idAgendamento);
+
+        if ($stmt->execute()) {
+            echo "Agendamento cancelado com sucesso.";
+        } else {
+            echo "Erro ao cancelar o agendamento: " . $stmt->error;
+        }
+
+        $stmt->close();
+    } else {
+        echo "Erro ao preparar a declaração: " . $mysqli->error;
+    }
+
+    $mysqli->close();
+} else {
+    echo "ID do agendamento não fornecido.";
+}
+
+// Redireciona de volta para a página de agendamentos
+header("Location: meus_agend_func.php");
+exit;
+?>
