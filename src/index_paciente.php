2bc0b8db60b9d3da584b74e0e19c362e00bb1d2c
caiogabriel2 <142699957+caiogabriel2@users.noreply.github.com>
2023-12-02 19:39:13 -0300

    Update index_paciente.php

--- src/index_paciente.php
+++ src/index_paciente.php
@@ -126,18 +126,6 @@ include 'autentica_paciente.php';
           </p>
         </div>
 
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4">
-            Acesso ao site
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Sobre</a>
-          </p>
-          
-        </div>
-
-        
-
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <h6 class="text-uppercase fw-bold mb-4" >Contato</h6>
           <p> <img src=".img/casa.png" class="img-fluid" ><i class="fas fa-casa me-3"></i> Rua Manoel Reis, 15 - Duque de Caxias- Rj </p>
