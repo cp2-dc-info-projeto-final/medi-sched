d65c43433851d774cacf5012e1d7ad59f5e5380e
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-12-02 19:45:07 -0300

    Update meus_agend.php

--- src/meus_agend.php
+++ src/meus_agend.php
@@ -64,7 +64,7 @@ $mysqli->close();
     <div id="header">
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
-                <a class="navbar-brand" href="index.php"><img src=".img/logo.png" class="img-center" width="45%"/></a>
+                <a class="navbar-brand" href="index_paciente.php"><img src=".img/logo.png" class="img-center" width="45%"/></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -76,8 +76,8 @@ $mysqli->close();
                         <li class="nav-item">
                             <a class="nav-link" href="perfil.php">Perfil</a>
                         </li>
-			            <li class="nav-item">
-				            <a class="nav-link" href="logout.php">Logout</a>
+			<li class="nav-item">
+				<a class="nav-link" href="logout.php">Logout</a>
                         </li>
                     </ul>
                 </div>
