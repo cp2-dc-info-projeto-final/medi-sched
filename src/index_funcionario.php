17f4bfad8bb8386152502ab04d4775f89615df73
caiogabriel2 <142699957+caiogabriel2@users.noreply.github.com>
2023-12-02 17:07:37 -0300

    Update index_funcionario.php

--- src/index_funcionario.php
+++ src/index_funcionario.php
@@ -29,12 +29,6 @@ include "autentica_funcionario.php";
                             <a class="nav-link active" aria-current="page" href="index_funcionario.php">Início</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="editar_agendamento.php">Editar Consultas</a> 
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="cancela_agendamento.php">Cancelar Consultas</a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link" href="meus_agend_func.php">Ver Agendamentos</a>
                         </li>
                         <li class="nav-item">
