dcbc185dda51b4bdd6f7f31daf449aacb208a023
caiogabriel2 <142699957+caiogabriel2@users.noreply.github.com>
2023-12-02 19:49:20 -0300

    Update editaemailadm.php

--- src/editaemailadm.php
+++ src/editaemailadm.php
@@ -1,11 +1,10 @@
 <!doctype html>
 <html lang="pt-br">
 <head>
-    <title>Área do Funcionário - Agenda+Saúde</title>
+    <title>Agenda+Saúde</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href=".css/editaemail.css"/>
-    <link rel="shortcut icon" href=".img/logo.png" type="image/x-icon" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
       integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
@@ -27,13 +26,13 @@
                             <a class="nav-link" href="cadastro_func.php">Cadastrar Funcionario</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="perfil_adm.php">Perfil</a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link" href="logout.php">Logout</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="editasenha.php">Redefinir Senha</a>
+                            <a class="nav-link" href="editaemailadm.php">Redefinir Email</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="editasenhaadm.php">Redefinir Senha</a>
                         </li>                       
                     </ul>
                 </div>
@@ -41,15 +40,22 @@
         </div>
     </div>
     <div class="container">
-        <p>Redefina seu Email!
-            Apos redefinir voce sera encaminhado direto para a pagina de login
-        </P>
-        <form action="editaemailadm.php" method="POST" class="form-container">
-          <input type="hidden" name="operacao" value="editemail">
-
-          <?php
-session_start();
+            <p>Redefina sua Senha!
+                Após redefinir você será encaminhado direto para a página de login.
+            </p>
+            <form action="editasenhaadm.php" method="POST" class="form-container">
+                <input type="hidden" name="operacao" value="editsenha">
+                
+                <p>Senha Atual:<input type="password" placeholder="Insira sua senha atual" name="senhaAtual"></p>
+                <p>Nova Senha:<input type="password" placeholder="Insira a nova senha" name="novaSenha"></p>
+                <p>Confirme a Nova Senha:<input type="password" placeholder="Confirme a nova senha" name="confirmaSenha"></p>
+                <p><input type="submit" value="Enviar" class="btn"></p>
+            </form>
+        </div>
+        <?php
 include "conecta_mysql.php";
+
+session_start();
 
 // Verifica se o usuário está logado
 if (!isset($_SESSION['email']) || !isset($_SESSION['tipo_usuario'])) {
@@ -62,29 +68,31 @@ $email = $_SESSION['email'];
 $tipo_usuario = $_SESSION['tipo_usuario'];
 
 // Verifica se o usuário é um administrador no banco de dados
-$stmt = $mysqli->prepare("SELECT * FROM Administrador WHERE email = ?");
+$stmt = $mysqli->prepare("SELECT * FROM administrador WHERE email = ?");
 $stmt->bind_param("s", $email);
 $stmt->execute();
 $result = $stmt->get_result();
 
-// Se não encontrar o usuário ou se o tipo na sessão não for 'administrador', redireciona
+// Se não encontrar o usuário ou se o tipo na sessão não for 'cliente', redireciona
 if ($result->num_rows != 1 || $tipo_usuario !== 'administrador') {
-    $_SESSION['erro_login'] = "Acesso restrito a administradores.";
+    $_SESSION['erro_login'] = "Acesso restrito a administrador.";
     header("Location: login.php");
     exit;
 }
 
-// A conexão com o banco de dados deve permanecer aberta para as operações seguintes
-// Não feche a conexão aqui
+$error_message = ''; // Variável para armazenar mensagens de erro
 
-$operacao = isset($_POST['operacao']) ? $_POST['operacao'] : '';
+// Processa a mudança de senha
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['operacao']) && $_POST['operacao'] === 'editsenha') {
 
-if ($operacao === "editemail") {
-    if (isset($_SESSION["email"])) {
-        $email = $_SESSION["email"];
-        $emailnovo = $mysqli->real_escape_string($_POST['emailnovo']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
+    $senhaAtual = mysqli_real_escape_string($mysqli, $_POST['senhaAtual']);
+    $novaSenha = mysqli_real_escape_string($mysqli, $_POST['novaSenha']);
+    $confirmaSenha = mysqli_real_escape_string($mysqli, $_POST['confirmaSenha']);
 
+    // Verifica se as senhas têm mais de 8 caracteres
+    if (strlen($novaSenha) < 8 || strlen($confirmaSenha) < 8) {
+        $error_message = "A senha deve ter pelo menos 8 caracteres.";
+    } else if ($novaSenha === $confirmaSenha) {
         $stmt = $mysqli->prepare("SELECT senha FROM administrador WHERE email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
@@ -93,46 +101,44 @@ if ($operacao === "editemail") {
         if ($result->num_rows > 0) {
             $row = $result->fetch_assoc();
 
-            if (password_verify($senha, $row['senha'])) {
-                $stmt = $mysqli->prepare("SELECT email FROM administrador WHERE email = ?");
-                $stmt->bind_param("s", $emailnovo);
-                $stmt->execute();
-                $result = $stmt->get_result();
+            if (password_verify($senhaAtual, $row['senha'])) {
+                $hashedNovaSenha = password_hash($novaSenha, PASSWORD_DEFAULT);
 
-                if ($result->num_rows === 0) {
-                    $stmt = $mysqli->prepare("UPDATE administrador SET email = ? WHERE email = ?");
-                    $stmt->bind_param("ss", $emailnovo, $email);
-                    if ($stmt->execute()) {
-                        $_SESSION["email"] = $emailnovo; // Atualiza a sessão
-                        header("Location: login.php"); // Redireciona para a página de login
-                        exit;
-                    } else {
-                        echo "Não foi possível atualizar o email.";
-                    }
+                $stmt = $mysqli->prepare("UPDATE administrador SET senha = ? WHERE email = ?");
+                $stmt->bind_param("ss", $hashedNovaSenha, $email);
+                $stmt->execute();
+
+                if ($stmt->affected_rows > 0) {
+                    // Senha atualizada com sucesso
+                    $_SESSION['mensagem_sucesso'] = "Senha atualizada com sucesso.";
+                    header("Location: login.php"); 
+                    exit;
                 } else {
-                    echo "E-mail já cadastrado.";
+                    $error_message = "Não foi possível atualizar a senha.";
                 }
             } else {
-                echo "Senha incorreta!";
+                $error_message = "Senha atual incorreta!";
             }
         } else {
-            echo "E-mail não encontrado.";
+            $error_message = "Erro ao buscar informações do usuário.";
         }
         $stmt->close();
+    } else {
+        $error_message = "As senhas não coincidem.";
     }
 }
 
-// Agora feche a conexão com o banco de dados
+// Feche a conexão com o banco de dados
 mysqli_close($mysqli);
+
+// Exibir mensagem de erro, se houver
+if (!empty($error_message)) {
+    echo "<p>Erro: " . $error_message . "</p>";
+}
+
 ?>
 
-
-        <p>Seu novo email<input type="email" placeholder="insira aqui" name="emailnovo"></p>
-          <p>Insira sua senha atual <input type="password" placeholder="insira aqui" name="senha"></p>
-          <p><input type="submit" value="Enviar" class="btn"></p>    
-        </form>
-
-    </div>
-
-  </body>
+</body>
 </html>
+
+
