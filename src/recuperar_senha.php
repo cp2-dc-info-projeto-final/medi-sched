9b273d72b5dc845e44a55564d65e9e6724966b1f
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-12-02 19:51:00 -0300

    Update recuperar_senha.php

--- src/recuperar_senha.php
+++ src/recuperar_senha.php
@@ -6,7 +6,7 @@ include "conecta_mysql.php";
 <!doctype html>
 <html lang="pt-br">
 <head>
-    <title>Esqueci a senha | Agenda+Saúde</title>
+    <title>Agenda+Saúde</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
