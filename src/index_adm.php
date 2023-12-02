b72be2ec0f67506bfc024f782286e3c59e605746
caiogabriel2 <142699957+caiogabriel2@users.noreply.github.com>
2023-12-02 17:12:03 -0300

    Update index_adm.php

--- src/index_adm.php
+++ src/index_adm.php
@@ -8,7 +8,7 @@ include "autentica_adm.php";
     <title>Área do Administrador - Agenda+Saúde</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href=""/>
+    <link rel="stylesheet" href=".css/inicial.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
       integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
