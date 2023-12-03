59d7741dbbd233df094b6641375747d2cf042a84
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-12-02 23:06:03 -0300

    Update perfil.php

--- src/perfil.php
+++ src/perfil.php
@@ -4,7 +4,7 @@
     <title>Agenda+Saúde</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href=".css/inicial.css"/>
+    <link rel="stylesheet" href=".css/perfil.css"/>
     <link rel="shortcut icon" href=".img/logo.png" type="image/x-icon" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
       integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
