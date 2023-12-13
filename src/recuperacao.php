0593e55a6e5e2458194ea75b8a224f34f66cd7c9
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-12-12 23:47:00 -0300

    Update recuperacao.php

--- src/recuperacao.php
+++ src/recuperacao.php
@@ -5,52 +5,55 @@ session_start();
 
 $email = $_POST['email'];
 
-// Verifica se o e-mail existe
-$query = "SELECT email FROM Cliente WHERE email = ?";
-$stmt = $mysqli->prepare($query);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
+// Gera uma nova senha aleatória
+$nova_senha = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 8);
+$senha_hash = password_hash($nova_senha, PASSWORD_DEFAULT);
 
-if ($result->num_rows === 0) {
-    // E-mail não encontrado
-    $_SESSION['msg_rec'] = "E-mail não encontrado no banco de dados.";
-    header("Location: login.php");
-    exit;
-} else {
-    // Gera uma nova senha aleatória
-    $nova_senha = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 8);
+$assunto = "Nova Senha Temporária";
+$mensagem = "Sua nova senha temporária é: $nova_senha";
 
-    $assunto = "Nova Senha Temporária";
-    $mensagem = "Sua nova senha temporária é: $nova_senha";
+// Função para atualizar a senha
+function atualizaSenha($mysqli, $email, $senha_hash, $tabela) {
+    $update_query = "UPDATE $tabela SET senha = ? WHERE email = ?";
+    $update_stmt = $mysqli->prepare($update_query);
+    $update_stmt->bind_param("ss", $senha_hash, $email);
+    $update_stmt->execute();
+    return $update_stmt->affected_rows > 0;
+}
 
-    // Começa a transação
-    $mysqli->begin_transaction();
+$tab_nomes = ["Cliente", "Funcionario", "Administrador"];
+$senhaAtualizada = false;
 
-    try {
-        // Atualiza a senha no banco de dados
-        $senha_hash = password_hash($nova_senha, PASSWORD_DEFAULT);
-        $update_query = "UPDATE Cliente SET senha = ? WHERE email = ?";
-        $update_stmt = $mysqli->prepare($update_query);
-        $update_stmt->bind_param("ss", $senha_hash, $email);
-        $update_stmt->execute();
+// Começa a transação
+$mysqli->begin_transaction();
 
-        if(envia_email($email, $assunto, $mensagem)) {
-            // Se o e-mail foi enviado com sucesso, commit na transação
-            $mysqli->commit();
-            $_SESSION['msg_rec'] = "Uma nova senha foi enviada para o seu e-mail.";
-        } else {
-            // Se falhar o envio do e-mail, rollback na transação
-            $mysqli->rollback();
-            $_SESSION['msg_rec'] = "Falha ao enviar e-mail.";
+try {
+    foreach ($tab_nomes as $tab_nome) {
+        $query = "SELECT email FROM $tab_nome WHERE email = ?";
+        $stmt = $mysqli->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            if (atualizaSenha($mysqli, $email, $senha_hash, $tab_nome)) {
+                $senhaAtualizada = true;
+                break;
+            }
         }
-    } catch (Exception $e) {
-        // Em caso de erro, rollback na transação
-        $mysqli->rollback();
-        $_SESSION['msg_rec'] = "Erro ao atualizar a senha.";
     }
 
-    header("Location: login.php");
-    exit;
+    if ($senhaAtualizada && envia_email($email, $assunto, $mensagem)) {
+        $mysqli->commit();
+        $_SESSION['msg_rec'] = "Uma nova senha foi enviada para o seu e-mail.";
+    } else {
+        $mysqli->rollback();
+        $_SESSION['msg_rec'] = "Falha ao enviar e-mail ou atualizar a senha.";
+    }
+} catch (Exception $e) {
+    $mysqli->rollback();
+    $_SESSION['msg_rec'] = "Erro ao atualizar a senha.";
 }
+
+
 ?>
