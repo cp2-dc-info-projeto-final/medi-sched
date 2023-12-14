d8f98a41f23310cb19af542ae873bd2742586505
MiguelmeSierra <95931280+MiguelmeSierra@users.noreply.github.com>
2023-12-14 00:01:25 -0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -72,3 +72,44 @@ INSERT INTO Administrador (
     '1985-01-01', 
     'Masculino'
 );
+
+-- Inserir cliente 1
+INSERT INTO Cliente (nome_cliente, sobrenome_cliente, email, senha, cpf, data_nascimento, genero)
+VALUES ('Maria', 'Silva', 'maria.silva@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '123.456.789-01', '1990-05-15', 'Feminino');
+
+-- Inserir cliente 2
+INSERT INTO Cliente (nome_cliente, sobrenome_cliente, email, senha, cpf, data_nascimento, genero)
+VALUES ('João', 'Pereira', 'joao.pereira@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '987.654.321-02', '1985-08-22', 'Masculino');
+
+-- Inserir cliente 3
+INSERT INTO Cliente (nome_cliente, sobrenome_cliente, email, senha, cpf, data_nascimento, genero)
+VALUES ('Ana', 'Santos', 'ana.santos@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '234.567.890-03', '1998-03-10', 'Feminino');
+
+-- Inserir cliente 4
+INSERT INTO Cliente (nome_cliente, sobrenome_cliente, email, senha, cpf, data_nascimento, genero)
+VALUES ('Carlos', 'Oliveira', 'carlos.oliveira@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '345.678.901-04', '1977-11-05', 'Masculino');
+
+-- Inserir cliente 5
+INSERT INTO Cliente (nome_cliente, sobrenome_cliente, email, senha, cpf, data_nascimento, genero)
+VALUES ('Mariana', 'Costa', 'mariana.costa@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '456.789.012-05', '1989-07-18', 'Feminino');
+
+-- Inserir funcionário 1 (Área: Ortopedia)
+INSERT INTO Funcionario (nome_funcionario, sobrenome_funcionario, email, senha, data_nascimento, cpf, area, genero)
+VALUES ('Carlos', 'Silva', 'carlos.silva@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '1980-08-10', '789.012.345-01', 'Ortopedia', 'Masculino');
+
+-- Inserir funcionário 2 (Área: Oftalmologia)
+INSERT INTO Funcionario (nome_funcionario, sobrenome_funcionario, email, senha, data_nascimento, cpf, area, genero)
+VALUES ('Ana', 'Pereira', 'ana.pereira@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '1992-05-22', '456.789.012-02', 'Oftalmologia', 'Feminino');
+
+-- Inserir funcionário 3 (Área: Odontologia)
+INSERT INTO Funcionario (nome_funcionario, sobrenome_funcionario, email, senha, data_nascimento, cpf, area, genero)
+VALUES ('João', 'Oliveira', 'joao.oliveira@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '1985-11-15', '123.456.789-03', 'Odontologia', 'Masculino');
+
+-- Inserir funcionário 4 (Área: Psicologia)
+INSERT INTO Funcionario (nome_funcionario, sobrenome_funcionario, email, senha, data_nascimento, cpf, area, genero)
+VALUES ('Mariana', 'Santos', 'mariana.santos@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '1990-02-28', '234.567.890-04', 'Psicologia', 'Feminino');
+
+-- Inserir funcionário 5 (Área: Ginecologia)
+INSERT INTO Funcionario (nome_funcionario, sobrenome_funcionario, email, senha, data_nascimento, cpf, area, genero)
+VALUES ('Pedro', 'Costa', 'pedro.costa@gmail.com', '$2y$10$VmyDO8OvY5BpsnMdRXi4q.CV4KCJ.zhXZkpgJjuTHJzmQAOtjoxG2', '1988-07-10', '345.678.901-05', 'Ginecologia', 'Masculino');
+
